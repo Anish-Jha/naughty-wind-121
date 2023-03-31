151c3392b103e774812ef6d674684541e3355b28
saikiran12040 <saikirangen@gmail.com>
2023-04-01 02:40:38 +0530

    added files

--- healthCare/frontend/src/App.js
+++ healthCare/frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Home from './Homepage/Home';
 import Navbar from './Navbar/Navbar'
 import SubNav from './Navbar/SubNav'
+import MainRoutes from './Routes/MainRoutes';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <SubNav/>
+    <MainRoutes/>
     <Home/>
     </div>
   );
