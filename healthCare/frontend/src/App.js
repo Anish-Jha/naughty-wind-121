d67ece6d4732f13a8e009debded11790ea3f93fc
Anish-Jha <anishj545@gmail.com>
2023-04-02 13:30:37 +0530

    day-5

--- healthCare/frontend/src/App.js
+++ healthCare/frontend/src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
-import Footer from "./Homepage/Footer/Footer";
+import Cart from "./Cart/Cart";
+import Footer from "./Footer/Footer";
 import Home from "./Homepage/Home";
 import Navbar from "./Navbar/Navbar";
-import SubNav from "./Navbar/SubNav";
+// import SubNav from "./Navbar/SubNav";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <SubNav />
-      <Home />
-      <Footer />
+      {/* <Navbar /> */}
+      <Cart/>
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ healthCare/frontend/src/Cart/CartCard.jsx
@@ -0,0 +1,52 @@
+import { Box, Button, Image, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+export const total=[];
+function CartCard({data,handleDicrement,handleIncrement,i,deleteCart}){
+    const cartData=JSON.parse(localStorage.getItem("cartitem"))||[]
+    const [count,setCount]=useState(1)
+    const handleCount=(val)=>{
+        setCount(count+val)
+    }
+
+    return(
+        <Box display={["","flex","flex","flex","flex","flex"]} w="95%" m="auto" justifyContent="space-between" borderTop="1px solid gray" borderBottom="1px solid gray" fontFamily="sans-serif" alignContent="center" alignItems="center">
+                <Box w={["50%","30%","30%","35%","35%","40%"]}  m="auto">
+                    <Text fontWeight="bold">Product</Text>
+                    <Box display="flex" gap="10px">
+                        <Box w={["90%","60%","50%","25%","20%","20%"]}>
+                            <Image src={data.image}/>
+                        </Box>
+                        <Box m="auto">
+                            <Text>{data.name}</Text>
+                            <Text>{data.subhead}</Text>
+                            <Button onClick={()=>deleteCart(i)}>Remove</Button>
+                        </Box>
+                    </Box>
+                </Box>
+
+                <Box>
+                    <Text fontWeight="bold">Price</Text>
+                    <Text>{data.price}</Text>
+                </Box>
+
+
+                <Box w="100px" m="auto">
+                    <Text fontWeight="bold">Quantity</Text>
+                    <Box display="flex" border="1px solid gray" bg="none">
+                        <Button bg="none" onClick={()=>handleIncrement(data.id)}>+</Button>
+                        <Text  m="auto">{data.quantity}</Text>
+                        <Button bg="none" isDisabled={data.quantity==1? true:false} onClick={()=>handleDicrement(data.id)}>-</Button>
+                    </Box>
+                </Box>
+
+                <Box fontWeight="bold">
+                    <Text >Total</Text>
+                    <Text>{data.quantity*data.price}</Text>
+
+                </Box>
+        </Box>
+        
+    )
+}
+export default CartCard;
